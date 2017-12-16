dd7abf07f292dbe0020ce03e71b9794ad71d6ef3
Carl Fee <15cfee@gmail.com>
2017-12-16 14:55:16 -0700

    reader

--- src/Solution.java
+++ src/Solution.java
@@ -6,16 +6,16 @@ import java.util.*;
 public class Solution {
     public static int n;
     public static int t;
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         // This is just generic stuff I use for hackerrank to make it easy to use IDE (reusing though)
-        File f = new File("sol.in");
-        Scanner in = new Scanner(System.in);
+        String fileName = "sol.in";
+        File f = new File(fileName);
+        Reader in = new Reader();
         if (f.exists() && !f.isDirectory()) {
-            in = new Scanner(new File("sol.in"));
+            in = new Reader(fileName);
         }
         /////////////////////// This is code I wrote for this problem
         long startTime = System.nanoTime();
-        System.out.println("Time: ");
         n = in.nextInt();
         int decSince = 0;
         int[] array = new int[n];
@@ -50,6 +50,8 @@ public class Solution {
             }
         }
         System.out.println(sumArray(array));
+        long endTime = System.nanoTime();
+        System.out.println("Total Run Time: " + (endTime - startTime)/1000);
 
 
         /////////////////////// Main End\\
@@ -184,6 +186,129 @@ class TupXY{
     TupXY(int first, int last){
         this.xT = first;
         this.yT = last;
+    }
+}
+
+
+// Copied the reader class from http://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+class Reader
+{
+    final private int BUFFER_SIZE = 1 << 16;
+    private DataInputStream din;
+    private byte[] buffer;
+    private int bufferPointer, bytesRead;
+
+    public Reader()
+    {
+        din = new DataInputStream(System.in);
+        buffer = new byte[BUFFER_SIZE];
+        bufferPointer = bytesRead = 0;
+    }
+
+    public Reader(String file_name) throws IOException
+    {
+        din = new DataInputStream(new FileInputStream(file_name));
+        buffer = new byte[BUFFER_SIZE];
+        bufferPointer = bytesRead = 0;
+    }
+
+    public String readLine() throws IOException
+    {
+        byte[] buf = new byte[64]; // line length
+        int cnt = 0, c;
+        while ((c = read()) != -1)
+        {
+            if (c == '\n')
+                break;
+            buf[cnt++] = (byte) c;
+        }
+        return new String(buf, 0, cnt);
+    }
+
+    public int nextInt() throws IOException
+    {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ')
+            c = read();
+        boolean neg = (c == '-');
+        if (neg)
+            c = read();
+        do
+        {
+            ret = ret * 10 + c - '0';
+        }  while ((c = read()) >= '0' && c <= '9');
+
+        if (neg)
+            return -ret;
+        return ret;
+    }
+
+    public long nextLong() throws IOException
+    {
+        long ret = 0;
+        byte c = read();
+        while (c <= ' ')
+            c = read();
+        boolean neg = (c == '-');
+        if (neg)
+            c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        }
+        while ((c = read()) >= '0' && c <= '9');
+        if (neg)
+            return -ret;
+        return ret;
+    }
+
+    public double nextDouble() throws IOException
+    {
+        double ret = 0, div = 1;
+        byte c = read();
+        while (c <= ' ')
+            c = read();
+        boolean neg = (c == '-');
+        if (neg)
+            c = read();
+
+        do {
+            ret = ret * 10 + c - '0';
+        }
+        while ((c = read()) >= '0' && c <= '9');
+
+        if (c == '.')
+        {
+            while ((c = read()) >= '0' && c <= '9')
+            {
+                ret += (c - '0') / (div *= 10);
+            }
+        }
+
+        if (neg)
+            return -ret;
+        return ret;
+    }
+
+    private void fillBuffer() throws IOException
+    {
+        bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+        if (bytesRead == -1)
+            buffer[0] = -1;
+    }
+
+    private byte read() throws IOException
+    {
+        if (bufferPointer == bytesRead)
+            fillBuffer();
+        return buffer[bufferPointer++];
+    }
+
+    public void close() throws IOException
+    {
+        if (din == null)
+            return;
+        din.close();
     }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////
